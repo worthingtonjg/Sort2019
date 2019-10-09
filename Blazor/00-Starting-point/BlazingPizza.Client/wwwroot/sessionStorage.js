373023f13f9d8d740b450eba653cc90be2bd6fd2
Jon Worthington <worthingtonjg@gmail.com>
2019-10-09 13:32:57 -0600

    last minute fix

---
+++ Blazor/00-Starting-point/BlazingPizza.Client/wwwroot/sessionStorage.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    window.blazorSessionStorage = {
+        get: key => key in sessionStorage ? JSON.parse(sessionStorage[key]) : null,
+        set: (key, value) => { sessionStorage[key] = JSON.stringify(value); },
+        delete: key => { delete sessionStorage[key]; }
+    };
+})();
---
+++ Blazor/final/BlazingPizza.Client/SessionStorage.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client
+{
+    public static class SessionStorage
+    {
+        public static Task<T> GetAsync<T>(IJSRuntime jsRuntime, string key)
+            => jsRuntime.InvokeAsync<T>("blazorSessionStorage.get", key);
+
+        public static Task SetAsync(IJSRuntime jsRuntime, string key, object value)
+            => jsRuntime.InvokeAsync<object>("blazorSessionStorage.set", key, value);
+
+        public static Task DeleteAsync(IJSRuntime jsRuntime, string key)
+            => jsRuntime.InvokeAsync<object>("blazorSessionStorage.delete", key);
+    }
+}
